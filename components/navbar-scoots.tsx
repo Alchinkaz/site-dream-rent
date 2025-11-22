6090a2e1e35153aefbeaef9a4bb01a6cbebe7ca6
username <mail@ex.com>
2025-11-22 13:30:31 +0500

    Исправлен текст под номером телефона в мобильной версии навбара: добавлен текст 'Звоните, мы в сети'

--- components/navbar-scoots.tsx
+++ components/navbar-scoots.tsx
@@ -146,7 +146,9 @@ export default function NavbarScoots() {
                   </a>
                   <div className="flex items-center space-x-2 mt-0">
                     <div className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></div>
-                    <span className="text-sm text-gray-500">Мы в сети с 10:00 до 20:00</span>
+                    <span className="text-sm font-medium whitespace-nowrap font-inter text-gray-500">
+                      Звоните, мы в сети
+                    </span>
                   </div>
                 </div>
 
